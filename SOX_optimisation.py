474dbb7fdcfd2227dd39a5dd8ee34c2fb04141e2
mdekauwe <mdekauwe@gmail.com>
2020-01-11 18:25:44 +1100

    first commit

---
+++ SOX_optimisation.py
@@ -0,0 +1,34 @@
+#!/usr/bin/env python
+
+"""
+Stomatal optimisation based on xylem hydraulics (SOX) model.
+
+Reference
+=========
+* Eller, C.B., Rowland, L., Mencuccini, M., Rosas, T., Williams, K., Harper, A.,
+  Medlyn, B.E., Wagner, Y., Klein, T., Teodoro, G.S., Oliveira, R.S.,
+  Matos, I.S., Rosado, B.H., Fuchs, K., Wohlfahrt, G., Montagnani, L., Meir, P.,
+  Sitch, S. and Cox, P.M. (2020), Stomatal optimisation based on xylem
+  hydraulics (SOX) improves land surface model simulation of vegetation
+  responses to climate. New Phytol., DOI: doi:10.1111/nph.16419.
+
+That's all folks.
+"""
+
+__author__ = "Martin De Kauwe"
+__version__ = "1.0 (11.01.2020)"
+__email__ = "mdekauwe@gmail.com"
+
+import os
+import sys
+import numpy as np
+import matplotlib.pyplot as plt
+import pandas as pd
+
+def main():
+    pass
+
+
+if __name__ == "__main__":
+
+    main()
